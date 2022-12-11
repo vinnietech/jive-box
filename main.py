4f5feede8063f9cc22c767257bb5f421b2691735
vinniecent <vincent@verbrugh.nl>
2022-12-11 20:10:49 +0100

    chore: import touch button function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from nfc_reader import read_nfc
+from touch_buttons import listen_to_touch_buttons
+
 
 def main():
     read_nfc()
+    listen_to_touch_buttons()
 
 if __name__ == "__main__":
     main()
