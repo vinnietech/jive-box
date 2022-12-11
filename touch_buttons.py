b349d3081df84d986285832824d1d47b6f2ad36e
vinniecent <vincent@verbrugh.nl>
2022-12-11 20:29:11 +0100

    fix: touch buttons

--- touch_buttons.py
+++ touch_buttons.py
@@ -4,16 +4,18 @@ import RPi.GPIO as GPIO
 
 import sonos
 
-TOUCH_SLEEP_TIME = 0.5
+TOUCH_SLEEP_TIME = 1
 
-PREVIOUS_TOUCH_PIN = 4
-PAUZE_PLAY_TOUCH_PIN = 17
+PREVIOUS_TOUCH_PIN = 23
+PAUZE_PLAY_TOUCH_PIN = 22
 NEXT_TOUCH_PIN = 27
+
 
 
 def touch_detect(pin):
     touch = GPIO.input(pin)
     return touch
+
 
 def listen_to_touch_buttons():
     GPIO.setmode(GPIO.BCM)
@@ -23,15 +25,18 @@ def listen_to_touch_buttons():
 
     while True:
         if touch_detect(PREVIOUS_TOUCH_PIN):
+            print("Previous touch detected")
             sonos.play_previous(os.environ.get('SONOS_DEVICE_IP'))
             time.sleep(TOUCH_SLEEP_TIME)
 
     while True:
         if touch_detect(NEXT_TOUCH_PIN):
+            print("Next touch detected")
             sonos.play_next(os.environ.get('SONOS_DEVICE_IP'))
             time.sleep(TOUCH_SLEEP_TIME)
 
     while True:
         if touch_detect(PAUZE_PLAY_TOUCH_PIN):
+            print("Play/pause touch detected")
             sonos.toggle_play_pause(os.environ.get('SONOS_DEVICE_IP'))
             time.sleep(TOUCH_SLEEP_TIME)
